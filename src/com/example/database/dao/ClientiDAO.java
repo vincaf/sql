44e42d00765755cde8c433748b69683d20063954
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2023-02-09 12:06:51 +0100

    feat: add ClientiDAO methods + test

--- src/com/example/database/dao/ClientiDAO.java
+++ src/com/example/database/dao/ClientiDAO.java
@@ -3,15 +3,24 @@ package com.example.database.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.example.database.entity.Cliente;
 
 public class ClientiDAO extends AbstractDAO<Cliente, Integer> {
 
+	private static Logger log = LogManager.getLogger();
+
 	private static final String findAllQuery = "SELECT ID_CLIENTE, NOME, COGNOME, EMAIL, INDIRIZZO, CITTA, PROVINCIA, CAP FROM clienti";
 	private static final String findByIdQuery = "SELECT ID_CLIENTE, NOME, COGNOME, EMAIL, INDIRIZZO, CITTA, PROVINCIA, CAP FROM clienti where ID_CLIENTE = ?";
+	private static final String countQuery = "select count(*) from clienti";
+	private static final String removeByIdQuery = "delete from clienti where ID_CLIENTE = ?";
+	private static final String insertQuery = "insert into clienti (NOME, COGNOME, EMAIL, INDIRIZZO, CITTA, PROVINCIA, CAP) values(?, ?, ?, ?, ?, ?, ?)";
 
 	@Override
 	public List<Cliente> findAll() {
@@ -64,10 +73,41 @@ public class ClientiDAO extends AbstractDAO<Cliente, Integer> {
 		return cliente;
 	}
 
+	/**
+	 * La insert crea un nuova tupla nel DB
+	 * ciò implica che l'istanza di Cliente sia incompleta perché idCliente non esiste (null);
+	 * l'idCliente, una volta volta inserita la tupla, mi serve, quindi devo effettuare una retrieve dell'id
+	 */
 	@Override
 	public Cliente create(Cliente entity) {
-		// TODO Auto-generated method stub
-		return null;
+		log.trace("Inizio inserimento del cliente {}", entity);
+		try (PreparedStatement pst = getConnection().prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);) {
+			preparazioneStatement(pst, entity);
+			int numeroDegliUpdates = pst.executeUpdate();
+			if(numeroDegliUpdates==0) {
+				throw new DAOException("Errore nell'inserimento di " + entity);
+			}
+			ResultSet keys = pst.getGeneratedKeys();
+			if(!keys.next()) {
+				throw new DAOException("Impossibile recuperare la chiave generata per " + entity);
+			}
+			entity.setIdCliente( keys.getInt(1) ); // imposto la chiave generata
+			log.debug("Cliente dopo inserimento ed assegnazione PK {}", entity);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("Errore durante la create()", e);
+		}
+		return entity;
+	}
+
+	private void preparazioneStatement(PreparedStatement pst, Cliente entity) throws SQLException {
+		pst.setString(1, entity.getNome());
+		pst.setString(2, entity.getCognome());
+		pst.setString(3, entity.getEmail());
+		pst.setString(4, entity.getIndirizzo());
+		pst.setString(5, entity.getCitta());
+		pst.setString(6, entity.getProvincia());
+		pst.setString(7, entity.getCap());
 	}
 
 	@Override
@@ -78,20 +118,35 @@ public class ClientiDAO extends AbstractDAO<Cliente, Integer> {
 
 	@Override
 	public Boolean removeById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		try (PreparedStatement pst = getConnection().prepareStatement(removeByIdQuery);) {
+			pst.setInt(1, id);
+			int numeroDegliUpdates = pst.executeUpdate();
+			return numeroDegliUpdates > 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("Errore durante la removeById()", e);
+		}
 	}
 
 	@Override
 	public Boolean remove(Cliente entity) {
-		// TODO Auto-generated method stub
-		return null;
+		return removeById(entity.getIdCliente());
 	}
 
 	@Override
 	public Long count() {
-		// TODO Auto-generated method stub
-		return null;
+		Long count = 0l; // boxing (da tipo primitivo a oggetto wrapper)
+		try (PreparedStatement pst = getConnection().prepareStatement(countQuery);
+				ResultSet rs = pst.executeQuery();
+				) {
+			if (rs.next()) {
+				count = rs.getLong(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("Errore durante la count()", e);
+		}
+		return count;
 	}
 
 }
--- src/com/example/database/dao/ClientiDAOTest.java
+++ src/com/example/database/dao/ClientiDAOTest.java
@@ -12,42 +12,61 @@ import com.example.database.entity.Cliente;
 class ClientiDAOTest {
 
 	@Test
+	void testCount() {
+		ClientiDAO dao = new ClientiDAO();
+		Long count = dao.count();
+		assert(count >= 0l);
+		System.out.println("count="+count);
+	}
+
+	@Test
 	void testFindAll() {
 		ClientiDAO dao = new ClientiDAO();
-
 		List<Cliente> clienti = dao.findAll();
-
 		assert(clienti.size() > 0);
-
 		clienti.forEach(System.out::println);
 	}
-	
+
 	@Test
 	void testFindById() {
 		ClientiDAO dao = new ClientiDAO();
-		
-		Cliente cliente = dao.findById(getRandomCliente().getIdCliente());
-
+		Cliente cliente = dao.findById( getRandomCliente().getIdCliente() );
 		assertTrue(cliente != null);
-
 		System.out.println(cliente);
 	}
 
 	@Test
 	void testFindById2() {
 		ClientiDAO dao = new ClientiDAO();
-		
 		Cliente cliente = dao.findById( -1 );
-
 		assertTrue(cliente == null);
-
-		System.out.println(cliente);
 	}
 
 	private Cliente getRandomCliente() {
 		ClientiDAO dao = new ClientiDAO();
 		List<Cliente> clienti = dao.findAll();
 		assert(clienti.size() > 0);
-		return clienti.get( new Random().nextInt(clienti.size() ) );
+		return clienti.get( new Random().nextInt( clienti.size() ) );
 	}
+
+	@Test
+	void testCreate() {
+		ClientiDAO dao = new ClientiDAO();
+		Cliente cliente = dao.create(generaNuovoCliente());
+		assert(cliente.getIdCliente() != null);
+		System.out.println("Inserito "+cliente);
+	}
+
+	private Cliente generaNuovoCliente() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("nuovo");
+		cliente.setCognome("cliente");
+		cliente.setEmail("nuovo.cliente@gmail.com");
+		cliente.setIndirizzo("Via della nazione, 22");
+		cliente.setCitta("Roma");
+		cliente.setProvincia("RM");
+		cliente.setCap("00100");
+		return cliente;
+	}
+
 }
